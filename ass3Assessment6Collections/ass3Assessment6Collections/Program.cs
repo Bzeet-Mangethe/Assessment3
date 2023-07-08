a607cee0e49ed27f39487dc8ee674ece40075d6d
Bhekani Masinga <bhekani.masinga@nagarro.com>
2023-07-08 19:32:23 +0200

    first commit

---
+++ ass3Assessment6Collections/ass3Assessment6Collections/Program.cs
@@ -0,0 +1,334 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+// Enum to represent the type of equipment
+enum EquipmentType
+{
+    Mobile,
+    Immobile
+}
+
+// Base Equipment class
+abstract class Equipment
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public double DistanceMovedTillDate { get; protected set; }
+    public decimal MaintenanceCost { get; protected set; }
+    public EquipmentType Type { get; protected set; }
+
+    public abstract void MoveBy(double distance);
+    public abstract void ShowDetails();
+}
+
+// Mobile Equipment class
+class MobileEquipment : Equipment
+{
+    public int NumberOfWheels { get; set; }
+
+    public MobileEquipment()
+    {
+        Type = EquipmentType.Mobile;
+    }
+
+    public override void MoveBy(double distance)
+    {
+        DistanceMovedTillDate += distance;
+        MaintenanceCost += NumberOfWheels * (decimal)distance;
+    }
+
+    public override void ShowDetails()
+    {
+        Console.WriteLine("Type: Mobile Equipment");
+        Console.WriteLine("Name: " + Name);
+        Console.WriteLine("Description: " + Description);
+        Console.WriteLine("Distance Moved Till Date: " + DistanceMovedTillDate + " km");
+        Console.WriteLine("Maintenance Cost: $" + MaintenanceCost);
+        Console.WriteLine("Number of Wheels: " + NumberOfWheels);
+    }
+}
+
+// Immobile Equipment class
+class ImmobileEquipment : Equipment
+{
+    public double Weight { get; set; }
+
+    public ImmobileEquipment()
+    {
+        Type = EquipmentType.Immobile;
+    }
+
+    public override void MoveBy(double distance)
+    {
+        DistanceMovedTillDate += distance;
+        MaintenanceCost += (decimal)(Weight * distance);
+    }
+
+    public override void ShowDetails()
+    {
+        Console.WriteLine("Type: Immobile Equipment");
+        Console.WriteLine("Name: " + Name);
+        Console.WriteLine("Description: " + Description);
+        Console.WriteLine("Distance Moved Till Date: " + DistanceMovedTillDate + " km");
+        Console.WriteLine("Maintenance Cost: $" + MaintenanceCost);
+        Console.WriteLine("Weight: " + Weight + " kg");
+    }
+}
+
+class Program
+{
+    static List<Equipment> equipmentList = new List<Equipment>();
+
+    static void Main()
+    {
+        bool exit = false;
+        while (!exit)
+        {
+            Console.WriteLine("========== Equipment Inventory ==========");
+            Console.WriteLine("1. Create an equipment");
+            Console.WriteLine("2. Delete an equipment");
+            Console.WriteLine("3. Move an equipment");
+            Console.WriteLine("4. List all equipment (basic details)");
+            Console.WriteLine("5. Show details of an equipment");
+            Console.WriteLine("6. List all mobile equipment");
+            Console.WriteLine("7. List all immobile equipment");
+            Console.WriteLine("8. List all equipment that have not been moved till now");
+            Console.WriteLine("9. Delete all equipment");
+            Console.WriteLine("10. Delete all immobile equipment");
+            Console.WriteLine("11. Delete all mobile equipment");
+            Console.WriteLine("0. Exit");
+            Console.WriteLine("=========================================");
+            Console.Write("Enter your choice: ");
+
+            if (int.TryParse(Console.ReadLine(), out int choice))
+            {
+                switch (choice)
+                {
+                    case 1:
+                        CreateEquipment();
+                        break;
+                    case 2:
+                        DeleteEquipment();
+                        break;
+                    case 3:
+                        MoveEquipment();
+                        break;
+                    case 4:
+                        ListAllEquipmentBasicDetails();
+                        break;
+                    case 5:
+                        ShowEquipmentDetails();
+                        break;
+                    case 6:
+                        ListAllMobileEquipment();
+                        break;
+                    case 7:
+                        ListAllImmobileEquipment();
+                        break;
+                    case 8:
+                        ListNotMovedEquipment();
+                        break;
+                    case 9:
+                        DeleteAllEquipment();
+                        break;
+                    case 10:
+                        DeleteAllImmobileEquipment();
+                        break;
+                    case 11:
+                        DeleteAllMobileEquipment();
+                        break;
+                    case 0:
+                        exit = true;
+                        Console.WriteLine("Exiting the program...");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid choice.");
+            }
+
+            Console.WriteLine();
+        }
+    }
+
+    static void CreateEquipment()
+    {
+        Console.WriteLine("Enter the details of the equipment:");
+        Console.Write("Name: ");
+        string name = Console.ReadLine();
+        Console.Write("Description: ");
+        string description = Console.ReadLine();
+
+        Equipment equipment;
+        Console.WriteLine("Choose the type of equipment:");
+        Console.WriteLine("1. Mobile Equipment");
+        Console.WriteLine("2. Immobile Equipment");
+        Console.Write("Enter your choice: ");
+        if (int.TryParse(Console.ReadLine(), out int typeChoice))
+        {
+            switch (typeChoice)
+            {
+                case 1:
+                    equipment = new MobileEquipment();
+                    Console.Write("Number of Wheels: ");
+                    if (int.TryParse(Console.ReadLine(), out int numberOfWheels))
+                    {
+                        ((MobileEquipment)equipment).NumberOfWheels = numberOfWheels;
+                        equipmentList.Add(equipment);
+                        Console.WriteLine("Mobile Equipment created successfully.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input for number of wheels.");
+                    }
+                    break;
+                case 2:
+                    equipment = new ImmobileEquipment();
+                    Console.Write("Weight (in kg): ");
+                    if (double.TryParse(Console.ReadLine(), out double weight))
+                    {
+                        ((ImmobileEquipment)equipment).Weight = weight;
+                        equipmentList.Add(equipment);
+                        Console.WriteLine("Immobile Equipment created successfully.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input for weight.");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Invalid type choice.");
+                    break;
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid input for type choice.");
+        }
+    }
+
+    static void DeleteEquipment()
+    {
+        Console.Write("Enter the name of the equipment to delete: ");
+        string name = Console.ReadLine();
+        Equipment equipment = equipmentList.FirstOrDefault(e => e.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
+        if (equipment != null)
+        {
+            equipmentList.Remove(equipment);
+            Console.WriteLine("Equipment deleted successfully.");
+        }
+        else
+        {
+            Console.WriteLine("Equipment not found.");
+        }
+    }
+
+    static void MoveEquipment()
+    {
+        Console.Write("Enter the name of the equipment to move: ");
+        string name = Console.ReadLine();
+        Equipment equipment = equipmentList.FirstOrDefault(e => e.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
+        if (equipment != null)
+        {
+            Console.Write("Enter the distance to move (in km): ");
+            if (double.TryParse(Console.ReadLine(), out double distance))
+            {
+                equipment.MoveBy(distance);
+                Console.WriteLine("Equipment moved successfully.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input for distance.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Equipment not found.");
+        }
+    }
+
+    static void ListAllEquipmentBasicDetails()
+    {
+        Console.WriteLine("Listing all equipment (basic details):");
+        foreach (Equipment equipment in equipmentList)
+        {
+            Console.WriteLine("Name: " + equipment.Name);
+            Console.WriteLine("Description: " + equipment.Description);
+            Console.WriteLine();
+        }
+    }
+
+    static void ShowEquipmentDetails()
+    {
+        Console.Write("Enter the name ofthe equipment to show details: ");
+        string name = Console.ReadLine();
+        Equipment equipment = equipmentList.FirstOrDefault(e => e.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
+        if (equipment != null)
+        {
+            equipment.ShowDetails();
+        }
+        else
+        {
+            Console.WriteLine("Equipment not found.");
+        }
+    }
+
+    static void ListAllMobileEquipment()
+    {
+        Console.WriteLine("Listing all mobile equipment:");
+        var mobileEquipment = equipmentList.Where(e => e.Type == EquipmentType.Mobile);
+        foreach (Equipment equipment in mobileEquipment)
+        {
+            Console.WriteLine("Name: " + equipment.Name);
+            Console.WriteLine("Description: " + equipment.Description);
+            Console.WriteLine();
+        }
+    }
+
+    static void ListAllImmobileEquipment()
+    {
+        Console.WriteLine("Listing all immobile equipment:");
+        var immobileEquipment = equipmentList.Where(e => e.Type == EquipmentType.Immobile);
+        foreach (Equipment equipment in immobileEquipment)
+        {
+            Console.WriteLine("Name: " + equipment.Name);
+            Console.WriteLine("Description: " + equipment.Description);
+            Console.WriteLine();
+        }
+    }
+
+    static void ListNotMovedEquipment()
+    {
+        Console.WriteLine("Listing all equipment that have not been moved till now:");
+        var notMovedEquipment = equipmentList.Where(e => e.DistanceMovedTillDate == 0);
+        foreach (Equipment equipment in notMovedEquipment)
+        {
+            Console.WriteLine("Name: " + equipment.Name);
+            Console.WriteLine("Description: " + equipment.Description);
+            Console.WriteLine();
+        }
+    }
+
+    static void DeleteAllEquipment()
+    {
+        equipmentList.Clear();
+        Console.WriteLine("All equipment deleted successfully.");
+    }
+
+    static void DeleteAllImmobileEquipment()
+    {
+        equipmentList.RemoveAll(e => e.Type == EquipmentType.Immobile);
+        Console.WriteLine("All immobile equipment deleted successfully.");
+    }
+
+    static void DeleteAllMobileEquipment()
+    {
+        equipmentList.RemoveAll(e => e.Type == EquipmentType.Mobile);
+        Console.WriteLine("All mobile equipment deleted successfully.");
+    }
+}
---
+++ ass3Assessment7/ass3Assessment7/Program.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+enum DuckType
+{
+    Rubber,
+    Mallard,
+    Redhead
+}
+
+interface IDuck
+{
+    string Name { get; set; }
+    double Weight { get; set; }
+    int NumberOfWings { get; set; }
+    DuckType Type { get; }
+    void Fly();
+    void Quack();
+}
+
+class RubberDuck : IDuck
+{
+    public string Name { get; set; }
+    public double Weight { get; set; }
+    public int NumberOfWings { get; set; }
+    public DuckType Type => DuckType.Rubber;
+
+    public void Fly()
+    {
+        Console.WriteLine("Rubber ducks don't fly.");
+    }
+
+    public void Quack()
+    {
+        Console.WriteLine("Rubber ducks squeak.");
+    }
+}
+
+class MallardDuck : IDuck
+{
+    public string Name { get; set; }
+    public double Weight { get; set; }
+    public int NumberOfWings { get; set; }
+    public DuckType Type => DuckType.Mallard;
+
+    public void Fly()
+    {
+        Console.WriteLine("Mallard ducks fly fast.");
+    }
+
+    public void Quack()
+    {
+        Console.WriteLine("Mallard ducks quack loud.");
+    }
+}
+
+class RedheadDuck : IDuck
+{
+    public string Name { get; set; }
+    public double Weight { get; set; }
+    public int NumberOfWings { get; set; }
+    public DuckType Type => DuckType.Redhead;
+
+    public void Fly()
+    {
+        Console.WriteLine("Redhead ducks fly slow.");
+    }
+
+    public void Quack()
+    {
+        Console.WriteLine("Redhead ducks quack mild.");
+    }
+}
+
+class DuckCollection : IEnumerable<IDuck>
+{
+    private List<IDuck> ducks;
+
+    public DuckCollection()
+    {
+        ducks = new List<IDuck>();
+    }
+
+    public void AddDuck(IDuck duck)
+    {
+        ducks.Add(duck);
+    }
+
+    public void RemoveDuck(IDuck duck)
+    {
+        ducks.Remove(duck);
+    }
+
+    public void RemoveAllDucks()
+    {
+        ducks.Clear();
+    }
+
+    public IEnumerator<IDuck> GetEnumerator()
+    {
+        return ducks.OrderBy(d => d.Weight).GetEnumerator();
+    }
+
+    System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public IEnumerable<IDuck> GetDucksByWings()
+    {
+        return ducks.OrderBy(d => d.NumberOfWings);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DuckCollection duckCollection = new DuckCollection();
+        bool exit = false;
+
+        while (!exit)
+        {
+            Console.WriteLine("Duck Simulation Game");
+            Console.WriteLine("1. Show Duck Collection");
+            Console.WriteLine("2. Add a Duck");
+            Console.WriteLine("3. Remove a Duck");
+            Console.WriteLine("4. Remove All Ducks");
+            Console.WriteLine("5. Iterate Ducks by Weight");
+            Console.WriteLine("6. Iterate Ducks by Number of Wings");
+            Console.WriteLine("0. Exit");
+            Console.Write("Enter your choice: ");
+
+            if (int.TryParse(Console.ReadLine(), out int choice))
+            {
+                switch (choice)
+                {
+                    case 1:
+                        ShowDuckCollection(duckCollection);
+                        break;
+                    case 2:
+                        AddDuck(duckCollection);
+                        break;
+                    case 3:
+                        RemoveDuck(duckCollection);
+                        break;
+                    case 4:
+                        RemoveAllDucks(duckCollection);
+                        break;
+                    case 5:
+                        IterateDucksByWeight(duckCollection);
+                        break;
+                    case 6:
+                        IterateDucksByWings(duckCollection);
+                        break;
+                    case 0:
+                        exit = true;
+                        Console.WriteLine("Goodbye!");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid choice. Please try again.");
+            }
+
+            Console.WriteLine();
+        }
+    }
+
+    static void ShowDuckCollection(DuckCollection duckCollection)
+    {
+        Console.WriteLine("Duck Collection:");
+        foreach (IDuck duck in duckCollection)
+        {
+            Console.WriteLine("Name: " + duck.Name);
+            Console.WriteLine("Weight: " + duck.Weight);
+            Console.WriteLine("Number of Wings: " + duck.NumberOfWings);
+            Console.WriteLine("Type: " + duck.Type);
+            Console.WriteLine();
+        }
+    }
+
+    static void AddDuck(DuckCollection duckCollection)
+    {
+        Console.WriteLine("Enter duck details:");
+        Console.Write("Name: ");
+        string name = Console.ReadLine();
+        Console.Write("Weight: ");
+        if (double.TryParse(Console.ReadLine(), out double weight))
+        {
+            Console.Write("Number of Wings: ");
+            if (int.TryParse(Console.ReadLine(), out int numberOfWings))
+            {
+                Console.Write("Duck Type (0 - Rubber, 1 - Mallard, 2 - Redhead): ");
+                if (Enum.TryParse<DuckType>(Console.ReadLine(), out DuckType type))
+                {
+                    IDuck duck;
+                    switch (type)
+                    {
+                        case DuckType.Rubber:
+                            duck = new RubberDuck();
+                            break;
+                        case DuckType.Mallard:
+                            duck = new MallardDuck();
+                            break;
+                        case DuckType.Redhead:
+                            duck = new RedheadDuck();
+                            break;
+                        default:
+                            duck = null;
+                            break;
+                    }
+
+                    if (duck != null)
+                    {
+                        duck.Name = name;
+                        duck.Weight = weight;
+                        duck.NumberOfWings = numberOfWings;
+                        duckCollection.AddDuck(duck);
+                        Console.WriteLine("Duck added successfully.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid duck type.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid duck type.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid number of wings.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid weight.");
+        }
+    }
+
+    static void RemoveDuck(DuckCollection duckCollection)
+    {
+        Console.Write("Enter the name of the duck to remove: ");
+        string name = Console.ReadLine();
+        IDuck duck = duckCollection.FirstOrDefault(d => d.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
+        if (duck != null)
+        {
+            duckCollection.RemoveDuck(duck);
+            Console.WriteLine("Duck removed successfully.");
+        }
+        else
+        {
+            Console.WriteLine("Duck not found.");
+        }
+    }
+
+    static void RemoveAllDucks(DuckCollection duckCollection)
+    {
+        duckCollection.RemoveAllDucks();
+        Console.WriteLine("All ducks removed successfully.");
+    }
+
+    static void IterateDucksByWeight(DuckCollection duckCollection)
+    {
+        Console.WriteLine("Iterating Ducks by Weight:");
+        foreach (IDuck duck in duckCollection)
+        {
+            Console.WriteLine("Name: " + duck.Name);
+            Console.WriteLine("Weight: " + duck.Weight);
+            Console.WriteLine();
+        }
+    }
+
+    static void IterateDucksByWings(DuckCollection duckCollection)
+    {
+        Console.WriteLine("Iterating Ducks by Number of Wings:");
+        foreach (IDuck duck in duckCollection.GetDucksByWings())
+        {
+            Console.WriteLine("Name: " + duck.Name);
+            Console.WriteLine("Number of Wings: " + duck.NumberOfWings);
+            Console.WriteLine();
+        }
+    }
+}
